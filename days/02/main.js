37043114b0dc1a5b449ae0c2485279a904775c86
Lâm Nguyen Hoang <lamnguyenhoang@gmail.com>
2020-12-28 16:36:04 +0100

    Day 2 part 2

--- days/02/main.js
+++ days/02/main.js
@@ -2,13 +2,21 @@ const { update, pipe, clone } = require('ramda')
 const { readInputOneLineWithNumbers } = require('../utils/readInput')
 
 function restoreGravityAssist (array) {
+  return patchArray(array, 12, 2)
+}
+
+function patchArray (array, noun, verb) {
   const clonedArray = clone(array)
-  clonedArray[1] = 12
-  clonedArray[2] = 2
+  clonedArray[1] = noun
+  clonedArray[2] = verb
   return clonedArray
 }
 
-function initializeProgram (array) { return { i: 0, program: array } }
+function initializeProgram (array) {
+  return { i: 0, program: array }
+}
+
+function extractAnswer (state) { return state.program[0] }
 
 function runProgram ({ i, program }) {
   switch (program[i]) {
@@ -16,8 +24,7 @@ function runProgram ({ i, program }) {
       return runProgram({
         i: i + 4,
         program: update(program[i + 3], program[program[i + 1]] + program[program[i + 2]], program)
-      }
-      )
+      })
     case 2:
       return runProgram({
         i: i + 4,
@@ -28,8 +35,21 @@ function runProgram ({ i, program }) {
   }
 }
 
+function findNounAndVerb (program) {
+  for (let noun = 0; noun < 99; noun++) {
+    for (let verb = 0; verb < 99; verb++) {
+      if (getProgramAnswer(patchArray(program, noun, verb)) === 19690720) {
+        return 100 * noun + verb
+      }
+    }
+  }
+  return null
+}
+
+const getProgramAnswer = pipe(initializeProgram, runProgram, extractAnswer)
+
 const INPUT_FILE = 'input.csv'
-const doPart1 = pipe(readInputOneLineWithNumbers, restoreGravityAssist, initializeProgram, runProgram)
-const doPart2 = pipe(readInputOneLineWithNumbers)
-console.log('part 1: ' + doPart1(__dirname, INPUT_FILE).program[0])
+const doPart1 = pipe(readInputOneLineWithNumbers, restoreGravityAssist, getProgramAnswer)
+const doPart2 = pipe(readInputOneLineWithNumbers, findNounAndVerb)
+console.log('part 1: ' + doPart1(__dirname, INPUT_FILE))
 console.log('part 2: ' + doPart2(__dirname, INPUT_FILE))
