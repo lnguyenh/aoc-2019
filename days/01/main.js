0c2593c3bdaca7cbe843812c652f515bed524aba
Lâm Nguyen Hoang <lamnguyenhoang@gmail.com>
2020-12-28 12:43:06 +0100

    Day 1 part 2

--- days/01/main.js
+++ days/01/main.js
@@ -3,20 +3,28 @@ const { readInput } = require('../utils/readInput')
 
 const INPUT_FILE = 'input.csv'
 
-function getFuel (masses) {
-  return masses.map((mass) => Math.floor(mass / 3) - 2)
+function getFuelsForMasses (masses) {
+  return masses.map((mass) => Math.floor(Number(mass) / 3) - 2)
 }
 
-function transformToNumber (textArray) {
-  return textArray.map((text) => Number(text))
+function getTotalFuel (fuelArray) {
+  const reduced = getFuelsForMasses(fuelArray).filter((v) => v > 0)
+  const total = sum(fuelArray)
+  if (reduced.length === 0) return total
+  return total + getTotalFuel(reduced)
 }
 
 const doPart1 = pipe(
   readInput,
-  transformToNumber,
-  getFuel,
+  getFuelsForMasses,
   sum
 )
 
+const doPart2 = pipe(
+  readInput,
+  getFuelsForMasses,
+  getTotalFuel
+)
+
 console.log('part 1: ' + doPart1(__dirname, INPUT_FILE))
-console.log('part 2: ' + '')
+console.log('part 2: ' + doPart2(__dirname, INPUT_FILE))
