00e38c5af3d001d4ec5de96593d52cad8f7b98c8
Lâm Nguyen Hoang <lamnguyenhoang@gmail.com>
2020-12-29 13:30:31 +0100

    Day 5 part 2

--- days/05/main.js
+++ days/05/main.js
@@ -3,7 +3,5 @@ const { runIntCode } = require('../intcode/engine')
 const { readInputOneLineWithNumbers } = require('../utils/readInput')
 
 const INPUT_FILE = 'input.csv'
-const doPart1 = pipe(readInputOneLineWithNumbers, runIntCode)
-const doPart2 = pipe(readInputOneLineWithNumbers)
-console.log('part 1: ' + doPart1(__dirname, INPUT_FILE))
-console.log('part 2: ' + doPart2(__dirname, INPUT_FILE))
+const doIt = pipe(readInputOneLineWithNumbers, runIntCode)
+doIt(__dirname, INPUT_FILE)
--- days/intcode/engine.js
+++ days/intcode/engine.js
@@ -15,8 +15,7 @@ function runProgram ({ i, program }) {
 
   const val1 = mode1 === '0' ? program[program[i + 1]] : program[i + 1]
   const val2 = mode2 === '0' ? program[program[i + 2]] : program[i + 2]
-
-  let input
+  const val3 = program[i + 3]
 
   switch (code) {
     case '01':
@@ -30,10 +29,9 @@ function runProgram ({ i, program }) {
         program: update(program[i + 3], val1 * val2, program)
       })
     case '03':
-      input = getInput()
       return runProgram({
         i: i + 2,
-        program: update(program[i + 1], input, program)
+        program: update(program[i + 1], getInput(), program)
       })
     case '04':
       console.log('Intcode output: ' + val1)
@@ -41,6 +39,54 @@ function runProgram ({ i, program }) {
         i: i + 2,
         program
       })
+    case '05':
+      if (val1 !== 0) {
+        return runProgram({
+          i: val2,
+          program
+        })
+      } else {
+        return runProgram({
+          i: i + 3,
+          program
+        })
+      }
+    case '06':
+      if (val1 === 0) {
+        return runProgram({
+          i: val2,
+          program
+        })
+      } else {
+        return runProgram({
+          i: i + 3,
+          program
+        })
+      }
+    case '07':
+      if (val1 < val2) {
+        return runProgram({
+          i: i + 4,
+          program: update(val3, 1, program)
+        })
+      } else {
+        return runProgram({
+          i: i + 4,
+          program: update(val3, 0, program)
+        })
+      }
+    case '08':
+      if (val1 === val2) {
+        return runProgram({
+          i: i + 4,
+          program: update(val3, 1, program)
+        })
+      } else {
+        return runProgram({
+          i: i + 4,
+          program: update(val3, 0, program)
+        })
+      }
     case '99':
       return { i, program }
   }
