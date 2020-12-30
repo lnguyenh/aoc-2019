b490019f4c24850fbb327de727ea46e64bae3761
Lâm Nguyen Hoang <lamnguyenhoang@gmail.com>
2020-12-30 15:32:45 +0100

    Refactor intcode using Ramda curry

--- intcode/engine.js
+++ intcode/engine.js
@@ -1,15 +1,11 @@
-const { update, pipe } = require('ramda')
+const { update, curry } = require('ramda')
 const readlineSync = require('readline-sync')
-
-function initializeProgram (array) {
-  return { i: 0, program: array }
-}
 
 function getUserInput () {
   return Number(readlineSync.question('Intcode input: '))
 }
 
-function runProgram ({ i, program }) {
+function runProgram (i, program) {
   const opCode = program[i].toString()
   const [, , mode2, mode1, code] = /^(\d)(\d)(\d)(\d\d)$/.exec(opCode.padStart(5, '0'))
 
@@ -19,79 +15,43 @@ function runProgram ({ i, program }) {
 
   switch (code) {
     case '01':
-      return runProgram({
-        i: i + 4,
-        program: update(program[i + 3], param1 + param2, program)
-      })
+      return runProgram(i + 4, update(program[i + 3], param1 + param2, program))
     case '02':
-      return runProgram({
-        i: i + 4,
-        program: update(program[i + 3], param1 * param2, program)
-      })
+      return runProgram(i + 4, update(program[i + 3], param1 * param2, program))
     case '03':
-      return runProgram({
-        i: i + 2,
-        program: update(program[i + 1], getUserInput(), program)
-      })
+      return runProgram(i + 2, update(program[i + 1], getUserInput(), program))
     case '04':
       console.log('Intcode output: ' + param1)
-      return runProgram({
-        i: i + 2,
-        program
-      })
+      return runProgram(i + 2, program)
     case '05':
       if (param1 !== 0) {
-        return runProgram({
-          i: param2,
-          program
-        })
+        return runProgram(param2, program)
       } else {
-        return runProgram({
-          i: i + 3,
-          program
-        })
+        return runProgram(i + 3, program)
       }
     case '06':
       if (param1 === 0) {
-        return runProgram({
-          i: param2,
-          program
-        })
+        return runProgram(param2, program)
       } else {
-        return runProgram({
-          i: i + 3,
-          program
-        })
+        return runProgram(i + 3, program)
       }
     case '07':
       if (param1 < param2) {
-        return runProgram({
-          i: i + 4,
-          program: update(param3, 1, program)
-        })
+        return runProgram(i + 4, update(param3, 1, program))
       } else {
-        return runProgram({
-          i: i + 4,
-          program: update(param3, 0, program)
-        })
+        return runProgram(i + 4, update(param3, 0, program))
       }
     case '08':
       if (param1 === param2) {
-        return runProgram({
-          i: i + 4,
-          program: update(param3, 1, program)
-        })
+        return runProgram(i + 4, update(param3, 1, program))
       } else {
-        return runProgram({
-          i: i + 4,
-          program: update(param3, 0, program)
-        })
+        return runProgram(i + 4, update(param3, 0, program))
       }
     case '99':
       return { i, program }
   }
 }
 
-const runIntCode = pipe(initializeProgram, runProgram)
+const runIntCode = curry(runProgram)(0)
 
 module.exports = { runIntCode }
