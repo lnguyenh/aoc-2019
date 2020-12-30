cfcb65e0c3a574f634f6bc445b4e56577634bc7d
Lâm Nguyen Hoang <lamnguyenhoang@gmail.com>
2020-12-30 15:25:46 +0100

    Day 6 part 2

--- days/06/main.js
+++ days/06/main.js
@@ -1,4 +1,4 @@
-const { pipe } = require('ramda')
+const { pipe, curry } = require('ramda')
 const { readLines } = require('../../inputReading/readInput')
 
 function splitByParenthesis (lines) {
@@ -6,18 +6,12 @@ function splitByParenthesis (lines) {
 }
 
 function createRelationships (array) {
-  const suns = array.reduce((suns, [sun, planet]) => {
-    if (suns.has(sun)) suns.get(sun).push(planet)
-    else suns.set(sun, [planet])
-    return suns
-  }, new Map())
-
   const planets = array.reduce((planets, [sun, planet]) => {
     planets.set(planet, sun)
     return planets
   }, new Map())
 
-  return { planets, suns }
+  return planets
 }
 
 function countSuns (planet, planets) {
@@ -25,7 +19,7 @@ function countSuns (planet, planets) {
   return 0
 }
 
-function countOrbits ({ planets, suns }) {
+function countOrbits (planets) {
   let numOrbits = 0
   for (const [planet, __] of planets) {
     numOrbits += countSuns(planet, planets)
@@ -33,7 +27,40 @@ function countOrbits ({ planets, suns }) {
   return numOrbits
 }
 
+const _getTransfersForPlanet = curry((distance, planet, planets) => {
+  if (planets.has(planet)) {
+    const transfers = _getTransfersForPlanet(distance + 1, planets.get(planet), planets)
+    transfers.set(planet, distance)
+    return transfers
+  } else {
+    // Last planet
+    return new Map()
+  }
+})
+
+const getTransfersForPlanet = _getTransfersForPlanet(-1)
+
+function getSantasAndMyTransfers (planets) {
+  const santasTransfers = getTransfersForPlanet('SAN', planets)
+  const myTransfers = getTransfersForPlanet('YOU', planets)
+  return [santasTransfers, myTransfers]
+}
+
+function getMinimumTransfers ([santasTransfers, myTransfers]) {
+  let meetingPoint = { sun: 'YOU', combinedDistance: Infinity }
+  for (const [sun, santasDistance] of santasTransfers) {
+    if (myTransfers.has(sun)) {
+      const combinedDistance = myTransfers.get(sun) + santasDistance
+      if (combinedDistance < meetingPoint.combinedDistance) {
+        meetingPoint = { sun, combinedDistance }
+      }
+    }
+  }
+  return meetingPoint.combinedDistance
+}
+
 const INPUT_FILE = 'input.csv'
 const doPart1 = pipe(readLines, splitByParenthesis, createRelationships, countOrbits)
+const doPart2 = pipe(readLines, splitByParenthesis, createRelationships, getSantasAndMyTransfers, getMinimumTransfers)
 console.log('part 1: ' + doPart1(__dirname, INPUT_FILE))
-console.log('toto')
+console.log('part 2: ' + doPart2(__dirname, INPUT_FILE))
